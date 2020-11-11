d73d4de11c4496ce18d2922f5acb39922307e68d
Monini1706 <64771796+Monini1706@users.noreply.github.com>
2020-11-11 19:39:21 +0100

    Create Player.java

---
+++ src/it/unive/dais/po1/exercise2/m876377/Player.java
@@ -0,0 +1,20 @@
+package it.unive.dais.po1.exercise2.m876377;
+
+import it.unive.dais.po1.exercise2.Board;
+import it.unive.dais.po1.exercise2.Mark;
+
+public class Player extends it.unive.dais.po1.exercise2.Player {
+    /**
+     * Intializes a player using the given Mark
+     *
+     * @param mark the mark of the player
+     */
+    public Player(Mark mark) {
+        super(mark);
+    }
+
+    @Override
+    public boolean play(Board board) {
+
+    }
+}
